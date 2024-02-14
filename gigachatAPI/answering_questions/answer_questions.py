55cb5955819bfdf5608c9953fe53d6b4f06e0541
Kostya Pershin <kostya.pershin.18@mail.ru>
2024-02-14 14:50:28 +0500

    Update answer_questions

--- gigachatAPI/answering_questions/answer_questions.py
+++ gigachatAPI/answering_questions/answer_questions.py
@@ -1,11 +1,12 @@
 import time
 from langchain.chains import LLMChain
+from langchain.chains import RetrievalQA
 from langchain.chat_models.gigachat import GigaChat
 from gigachatAPI.config_data.config_data import *
 from gigachatAPI.utils.create_prompts import create_prompt
 from gigachatAPI.config_data.config import load_config, Config
 from gigachatAPI.dita_case.scrap_files import get_dita_docs
-from gigachatAPI.answering_questions.docs_filtration import filter_docs, filter_docs2
+from gigachatAPI.answering_questions.docs_filtration import filter_docs
 from gigachatAPI.utils.split_docs import get_docs_list
 
 
@@ -15,7 +16,7 @@ def get_answer(file_path: str, question_list: list[str], dita: int = 0, after_qu
     giga: GigaChat = GigaChat(credentials=config.GIGA_CREDENTIALS, verify_ssl_certs=False)
 
     if dita == 1:
-        split_docs = get_dita_docs(file_path, chunk_size=25000)
+        split_docs = get_dita_docs(file_path, chunk_size=10000)
         func = filter_docs
         out_files_num = 1
     else:
@@ -26,19 +27,20 @@ def get_answer(file_path: str, question_list: list[str], dita: int = 0, after_qu
     print(f'[INFO] Всего вопросов: {len(question_list)} | Всего документов: {len(split_docs)} | Длина документов:'
           f' {sum(len(i.page_content) for i in split_docs)}\n')
 
-    prompt = create_prompt(get_answ_sys_prompt_path, get_answ_usr_prompt_path)
-
-    chain = LLMChain(llm=giga, prompt=prompt)
+    # prompt = create_prompt(get_answ_sys_prompt_path, get_answ_usr_prompt_path)
 
     final_res = ''
     for q_num, que in enumerate(question_list, start=1):
         start_time = time.time()
-        filtered_docs = func(split_docs, que, out_files_num=out_files_num)
+        filtered_docs, retriever = func(split_docs, que, out_files_num=out_files_num)
+        chain = RetrievalQA.from_chain_type(llm=giga, retriever=retriever.as_retriever())
         print(f'[INFO] Документы отфильтрованы за {time.time() - start_time}')
         if dita == 1:
             dita_length = sum(len(i.page_content) for i in filtered_docs)
             print(f'[INFO] Длина отфильтрованных документов: {dita_length}')
-        result = chain.run(question=que, summaries=filtered_docs)
+        # result = chain.run(question=que, summaries=filtered_docs)
+        qa_chain = chain({"query": que})
+        result = qa_chain['result']
         if after_que:
             final_res += f'{q_num}. {result}\n\n'
         else:
--- gigachatAPI/answering_questions/docs_filtration.py
+++ gigachatAPI/answering_questions/docs_filtration.py
@@ -2,24 +2,40 @@ from typing import Any
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.embeddings.gpt4all import GPT4AllEmbeddings
 from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.storage import InMemoryStore
-from langchain.retrievers.multi_vector import MultiVectorRetriever
+from chromadb.config import Settings
 
 
 def filter_docs(split_docs: list[Document], question: str, out_files_num=4) -> Any:
-    vectorstore = Chroma.from_documents(documents=split_docs, embedding=SentenceTransformerEmbeddings())
+    vectorstore = Chroma.from_documents(documents=split_docs,
+                                        embedding=SentenceTransformerEmbeddings(),
+                                        client_settings=Settings(anonymized_telemetry=False),)
     docs = vectorstore.similarity_search(question, k=out_files_num)
 
-    return docs
+    return docs, vectorstore
 
 
-def filter_docs2(split_docs: list[Document], question: str, out_files_num=4) -> Any:
-    vectorstore = Chroma.from_documents(documents=split_docs, embedding=GPT4AllEmbeddings())
-    docs = vectorstore.similarity_search(question, k=out_files_num)
+# def filter_docs2(split_docs: list[Document], question: str, out_files_num=4) -> Any:
+#     vectorstore = Chroma.from_documents(documents=split_docs, embedding=GPT4AllEmbeddings())
+#     docs = vectorstore.similarity_search(question, k=out_files_num)
+#
+#     return docs
 
-    return docs
+
+# def filter_docs_new(split_docs: list[Document], question: str, out_files_num=4) -> Any:
+#     vectorizer = TfidfVectorizer()
+#
+#     split_docs = list(map(lambda x: x.page_content, split_docs))
+#
+#     tfidf_matrix = vectorizer.fit_transform(split_docs + [question])
+#
+#     cosine_similarities = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1]).flatten()
+#
+#     tfidf_matches = sorted(enumerate(cosine_similarities), key=lambda x: x[1], reverse=True)
+#
+#     docs = list(map(lambda x: Document(page_content=split_docs[x[0]]), tfidf_matches[:out_files_num]))
+#
+#     return docs
 
 
 # def retrieve_docs(split_docs: list[Document], question: str) -> Any:
